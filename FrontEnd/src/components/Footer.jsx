1802c1257cff4ad722190b18d475c42890944c5b
MaruFerraro <ferraro.mariaeugenia@gmail.com>
2022-10-22 19:17:57 -0300

    adding responsive header, body and footer

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -4,22 +4,21 @@ import facebook   from "../resources/facebook.svg"
 import instagram   from "../resources/instagram.svg"
 import linkedin   from "../resources/linkedin.svg"
 
-
 const Footer = () => {
-  return (
-    <footer className='h-[58px] bg-[#1DBEB4] w-full bottom-0  fixed text-white flex flex-row justify-between items-center px-10 '> 
-        <p >©2021 Digital Booking</p>
-        <div className='hidden md:block'>
-            <ul className='flex flex-row'>
-                <li className='mx-[10px]'><img src={facebook}  alt='facebook'></img></li>
-                <li className='mx-[10px]'><img src={linkedin}  alt='linkedin'></img></li>
-                <li className='mx-[10px]'><img src={tweeter}   alt='tweeter'></img></li>
-                <li className='mx-[10px]'><img src={instagram} alt='instagram'></img></li>
-            </ul>
-        </div>
-    </footer>
-    
-  )
-}
+    return (
+      <footer> 
+          <p >©2021 Digital Booking</p>
+          <div className='md-screen'>
+              <ul>
+                  <li ><img src={facebook}  alt='facebook'></img></li>
+                  <li ><img src={linkedin}  alt='linkedin'></img></li>
+                  <li ><img src={tweeter}   alt='tweeter'></img></li>
+                  <li ><img src={instagram} alt='instagram'></img></li>
+              </ul>
+          </div>
+      </footer>
+      
+    )
+  }
 
 export default Footer
