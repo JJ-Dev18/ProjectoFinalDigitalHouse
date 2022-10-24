46470af508f4c25ee34690188360f2b7c4af9d20
jriveralopez <jairorive96@gmail.com>
2022-10-24 18:59:36 -0500

    adding categories from private repo(made by Juan)

---
+++ FrontEnd/src/components/CategoryBlock.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Categories from '../resources/categories.json'
+import img_categoria from '../resources/img_categoria.png'
+
+export const CategoryBlock = () => {
+ 
+  return (
+    <div className="category_block">
+      <h2>Buscar por tipo de alojamiento</h2>
+      {/* <p>Paragrafo</p> */}
+      <ul className='list_categories'>
+        {Categories.map((category, index) => (
+          <li key={index} className="category_card">
+            <img src={img_categoria} alt="" />
+            <div className='info_card_category'>
+            <h2>{category.name}</h2>
+            <p>{category.cant}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
