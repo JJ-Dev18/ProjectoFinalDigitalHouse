cfe50f7bf392ff51e821774c85285f72223ea73a
JJ-Dev18 <jj.murillo182020@gmail.com>
2022-11-03 14:39:46 -0500

    characteristics add to product-detail

--- FrontEnd/src/components/ProductDetail.jsx
+++ FrontEnd/src/components/ProductDetail.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import ProductHeader from './ProductHeader';
 import ProductGallery from './ProductGallery';
 import './styles/product-detail.css';
+import Characteristics from './Characteristics';
 
 const ProductDetail = () => {
   return (
-    <div className='product-detail-container'>
-      <ProductHeader category={'Hotel'} name={'Hotel Hermitage'} />
-      <ProductGallery images={['TODO']} />
+    <div className="product-detail-container">
+      <ProductHeader category={"Hotel"} name={"Hotel Hermitage"} />
+      <ProductGallery images={["TODO"]} />
       <div> {`<product-description/>`}</div>
-      <div> {`<product-characteristics/>`}</div>
+      <Characteristics />
       <div> {`<product-policies/>`}</div>
     </div>
-  )
+  );
 }
 
 export default ProductDetail;
