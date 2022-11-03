69fad22af48dd32bdd5a262105ca26e65fbac9b2
jriveralopez <jairorive96@gmail.com>
2022-11-02 19:59:54 -0500

    Fixing error when devs pull into the branch

---
+++ BackEnd/DHBooking/src/main/java/Grupo7/DHBooking/Entities/Role.java
@@ -0,0 +1,31 @@
+package Grupo7.DHBooking.Entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_role")
+    private Long idRole;
+
+    @Column
+    private String name;
+
+    @JsonManagedReference
+    @JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "role")
+    private List<User> users;
+
+
+}
--- BackEnd/hoteles/src/main/java/com/hoteles/hoteles/Repository/ICategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hoteles.hoteles.Repository;
-
-import com.hoteles.hoteles.Entities.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICategoryRepository extends JpaRepository<Category, Long> {
-}
