2d47ed3ee250050738a59c8559c0c2b935513082
JJ-Dev18 <jj.murillo182020@gmail.com>
2022-11-03 21:46:01 -0500

    policies phone done

--- FrontEnd/src/components/ProductDetail.jsx
+++ FrontEnd/src/components/ProductDetail.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import ProductHeader from './ProductHeader';
-import ProductGallery from './ProductGallery';
-import './styles/product-detail.css';
-import Characteristics from './Characteristics';
+import React from "react";
+import ProductHeader from "./ProductHeader";
+import ProductGallery from "./ProductGallery";
+import "./styles/product-detail.css";
+import Characteristics from "./ProductCharacteristics";
+import ProductCharacteristics from "./ProductCharacteristics";
+import ProductPolicies from "./ProductPolicies";
 
 const ProductDetail = () => {
   return (
@@ -10,10 +12,10 @@ const ProductDetail = () => {
       <ProductHeader category={"Hotel"} name={"Hotel Hermitage"} />
       <ProductGallery images={["TODO"]} />
       <div> {`<product-description/>`}</div>
-      <Characteristics />
-      <div> {`<product-policies/>`}</div>
+      <ProductCharacteristics />
+      <ProductPolicies/>
     </div>
   );
-}
+};
 
 export default ProductDetail;
