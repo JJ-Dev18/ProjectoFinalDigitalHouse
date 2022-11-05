9818f7ddc8d9bc4e93b2d35da3c8df8a8279260a
jriveralopez <jairorive96@gmail.com>
2022-11-04 19:01:47 -0500

    cleaning branch by deleting back end folder and adjusting testing study cases excel

--- BackEnd/hoteles/src/main/java/com/hoteles/hoteles/Repository/IUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hoteles.hoteles.Repository;
-
-import com.hoteles.hoteles.Entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserRepository extends JpaRepository<User, Long> {
-}
