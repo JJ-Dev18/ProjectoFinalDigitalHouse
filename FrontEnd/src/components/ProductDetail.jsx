54bdcb7a4fdb940f23afdb9a7404ddc7c8ca22bb
JJ-Dev18 <jj.murillo182020@gmail.com>
2022-11-06 22:27:18 -0500

    product description done

--- FrontEnd/src/components/ProductDetail.jsx
+++ FrontEnd/src/components/ProductDetail.jsx
@@ -6,16 +6,24 @@ import Characteristics from "./ProductCharacteristics";
 import ProductCharacteristics from "./ProductCharacteristics";
 import ProductPolicies from "./ProductPolicies";
 import ProductReservation from "./ProductReservation";
-
+import ProductDescription from "./ProductDescription";
+import product from '../resources/products.json'
 const ProductDetail = () => {
   return (
     <div className="product-detail-container">
       <ProductHeader category={"Hotel"} name={"Hotel Hermitage"} />
       <ProductGallery images={["TODO"]} />
-      <div> {`<product-description/>`}</div>
-      <ProductCharacteristics />
+      <ProductDescription
+        title={product[0].titleDescription}
+        description={product[0].category.description}
+      />
+      <ProductCharacteristics characteristics={product[0].characteristics} />
       <ProductReservation />
-      <ProductPolicies/>
+      <ProductPolicies
+        normsPolicy={product[0].NormsPolicy}
+        securityPolicy={product[0].SecurityPolicy}
+        cancellationPolicy={product[0].CancellationPolicy}
+      />
     </div>
   );
 };
