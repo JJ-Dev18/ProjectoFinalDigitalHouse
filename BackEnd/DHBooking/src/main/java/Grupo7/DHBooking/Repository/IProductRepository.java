20ebfb70e726374acb383bc40ff959403592735b
jriveralopez <jairorive96@gmail.com>
2022-11-07 19:00:15 -0500

    Adding cors and repository annotations

--- BackEnd/DHBooking/src/main/java/Grupo7/DHBooking/Repository/IProductRepository.java
+++ BackEnd/DHBooking/src/main/java/Grupo7/DHBooking/Repository/IProductRepository.java
@@ -2,6 +2,8 @@ package Grupo7.DHBooking.Repository;
 
 import Grupo7.DHBooking.Entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IProductRepository extends JpaRepository<Product, Long> {
 }
