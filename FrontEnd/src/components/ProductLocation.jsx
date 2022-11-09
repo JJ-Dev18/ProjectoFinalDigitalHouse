a26807984f91ae61f715bf24c24dfbed9ef39067
ruth-bmendez <ruthbordam@gmail.com>
2022-11-09 10:56:36 -0300

    Modifying components because the api changed

--- FrontEnd/src/components/ProductLocation.jsx
+++ FrontEnd/src/components/ProductLocation.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import './styles/product-location.css';
 import PinIcon from '../resources/gps.svg';
 
-const ProductLocation = ({ city, province, country, distance }) => {
+const ProductLocation = ({ name, state, country, distance }) => {
   return (
     <div className='product-location-container'>
       <img className='pin-icon' src={PinIcon} alt="Pin Icon" />
       <div className='product-location'>
-        <span>{province}, </span>
-        <span>{city}, </span>
+        <span>{state}, </span>
+        <span>{name}, </span>
         <span>{country}. </span>
         {false && <div>{distance}</div>}
       </div>
