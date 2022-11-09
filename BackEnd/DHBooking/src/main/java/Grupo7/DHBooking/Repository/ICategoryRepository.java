5678ebe9962623d30ea3385fbd3240d776aa6704
darcila06 <darcilaibague@gmail.com>
2022-11-09 16:40:52 -0500

    calendar_fixes

--- BackEnd/DHBooking/src/main/java/Grupo7/DHBooking/Repository/ICategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package Grupo7.DHBooking.Repository;
-
-import Grupo7.DHBooking.Entities.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICategoryRepository extends JpaRepository<Category, Long> {
-}
--- BackEnd/DHBooking/src/main/java/Grupo7/DHBooking/Repository/IUserRepository.java
+++
@@ -1,7 +0,0 @@
-package Grupo7.DHBooking.Repository;
-
-import Grupo7.DHBooking.Entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserRepository extends JpaRepository<User, Long> {
-}
