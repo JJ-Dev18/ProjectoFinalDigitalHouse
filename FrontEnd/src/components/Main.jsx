01e696a4abaad2ca618e19f354f37ed392d70ad4
JJ-Dev18 <jj.murillo182020@gmail.com>
2022-11-09 16:41:47 -0500

    usefetch v2 change and products api almost done

--- FrontEnd/src/components/Main.jsx
+++ FrontEnd/src/components/Main.jsx
@@ -1,18 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/main.css";
 import SearchBlock from "./SearchBlock";
 import CategoryBlock from "./CategoryBlock";
 import Recommended from "./Recommended";
 import Characteristics from "./ProductCharacteristics";
 import FetchData from "./FetchData";
+// import useFetch from "../hooks/useFetch";
+import baseURL, { backendApi } from "../hooks/axiosBase";
+import recommended from "../resources/recommended.json";
+import { useEffect } from "react";
+import useFetch from "../hooks/useFetch";
 
 const Main = (props) => {
+  const [categorSelected, setcategory] = useState(0);
+  const { response: categories, isLoading } = useFetch({
+    api: backendApi,
+    method: "get",
+    url: "/categories",
+  });
+  const { response: products, isLoading: isLoadingProducts } = useFetch({
+    api: backendApi,
+    method: "get",
+    url: "/products",
+  });
+  console.log(products);
+  // const {
+  //   isLoading: isLoadingCategories,
+  //   errorMessage: errorMessageProducts,
+  //   apiData: categories,
+  // } = useFetch(baseURL + "categories");
+  // const {
+  //   isLoading,
+  //   errorMessage,
+  //   apiData: products,
+  // } = useFetch(baseURL + "products");
+
+  // console.log(isLoading, products);
+  useEffect(() => {}, []);
+
   return (
     <main>
       <SearchBlock />
       {/* <FetchData /> */}
-      <CategoryBlock />
-      <Recommended />
+      {!isLoading ? (
+        <CategoryBlock categories={categories} setcategory={setcategory} />
+      ) : (
+        <h1>Loading ....</h1>
+      )}
+      {!isLoadingProducts ? (
+        <Recommended products={products} />
+      ) : (
+        <h1>Loading ....</h1>
+      )}
     </main>
   );
 };
--- FrontEnd/src/hooks/axiosBase.js
+++ FrontEnd/src/hooks/axiosBase.js
@@ -1,8 +1,8 @@
-// import axios from "axios";
+import axios from "axios";
 
-// export default axios.create({
-//     baseURL: "http://52.14.228.70/DHBooking/",
-//     });
+export const backendApi = axios.create({
+    baseURL: "http://52.14.228.70/DHBooking/",
+ });
 
 
 const baseURL = "http://52.14.228.70/DHBooking/";
