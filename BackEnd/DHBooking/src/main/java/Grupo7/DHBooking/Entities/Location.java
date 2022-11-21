db4ee83c74bc9205b9a845935aa67f61c7c07462
MaruFerraro <ferraro.mariaeugenia@gmail.com>
2022-11-21 18:26:39 -0300

    address entity

---
+++ BackEnd/DHBooking/src/main/java/Grupo7/DHBooking/Entities/Location.java
@@ -0,0 +1,28 @@
+package Grupo7.DHBooking.Entities;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "locations")
+@Getter
+@Setter
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_location")
+    private Long idLocation;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+    @Column(name = "address")
+    private String address;
+}
