1326c4d11ebfe0003690ccce8ecda586b8b92e1f
jriveralopez <jairorive96@gmail.com>
2022-11-23 21:03:23 -0500

    Final solution problem

--- BackEnd/DHBooking/src/main/java/Grupo7/DHBooking/Service/impl/BookingServiceImpl.java
+++ BackEnd/DHBooking/src/main/java/Grupo7/DHBooking/Service/impl/BookingServiceImpl.java
@@ -85,9 +85,19 @@ public class BookingServiceImpl implements IBookingService {
     }
 
     private LocalDate parseDocumentDateFormat(String date) throws ParseException {
-        DateTimeFormatter queryFormat = DateTimeFormatter.ofPattern("M/d/yyyy", Locale.US);
-        LocalDate queryDate = LocalDate.parse(date, queryFormat);
+        String[] separatedString = date.split("/");
+        Integer day = Integer.parseInt(separatedString[0]);
+        Integer month = Integer.parseInt(separatedString[1]);
 
+        DateTimeFormatter queryFormat;
+
+        if(day < 10 && month < 10){
+            queryFormat = DateTimeFormatter.ofPattern("M/d/yyyy", Locale.US);
+        } else {
+            queryFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy", Locale.US);
+        }
+
+        LocalDate queryDate = LocalDate.parse(date, queryFormat);
         DateTimeFormatter sqlDateForm = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         String stringSQLDate = queryDate.format(sqlDateForm);
 
