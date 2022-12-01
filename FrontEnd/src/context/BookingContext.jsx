a16ba95ea9d2f1ba546037f4ee5af75b8fb3a7b4
MaruFerraro <ferraro.mariaeugenia@gmail.com>
2022-12-01 10:28:56 -0300

    filter by city and date solved

--- FrontEnd/src/context/BookingContext.jsx
+++ FrontEnd/src/context/BookingContext.jsx
@@ -5,7 +5,7 @@ const BookingContext = React.createContext({})
 
 export function BookingProvider ({children}) {
   const [range, setRange] = useState([null, null]);
-  const [city, setCity] = useState("")
+  const [city, setCity] = useState(null)
   return <BookingContext.Provider value={{range, setRange,city,setCity}}>
     {children}
   </BookingContext.Provider>
