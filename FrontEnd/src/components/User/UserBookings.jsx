8d3f5779842a01481a1393c975d6b3c50f6cc765
MaruFerraro <ferraro.mariaeugenia@gmail.com>
2022-12-03 14:49:20 -0300

    mu bookings styles

--- FrontEnd/src/components/User/UserBookings.jsx
+++ FrontEnd/src/components/User/UserBookings.jsx
@@ -9,24 +9,31 @@ import { TbClock2 } from "react-icons/tb";
 const UserBookings = () => {
 
     const { userAuth } = useContext(AuthContext);
-
+    // ${showMenu ? `active` : `inactive`}`
     return (
     <div className="user-bookings">
         <h1>Mis reservas</h1>
         {userAuth.bookingList.map(item => (
-            <div className="card">
-                <div className="card-header">
-                <h3>{item.product.title}</h3>
+            <div>
+                <div className="booking-state">
+                    <p>{new Date(item.endDate).setDate(new Date(item.endDate).getDate()) < (new Date()) ? "Realizada" : "Vigente"}</p>
                 </div>
-                <div className="card-body">
-                    <p className="card-body-item"> <span> <RiLoginCircleFill className="card-body-item-icon in" />  Check-in: </span> {item.startDate}</p>
-                    <p className="card-body-item"> <span> <TbClock2 className="card-body-item-icon"  /> Ingreso: </span> {item.startHour} Hs</p>
-                    <p className="card-body-item"> <span> <RiLogoutCircleFill className="card-body-item-icon out"  /> Check-out: </span> {item.endDate}</p>
-                </div>
-                <div className="card-footer">
-                <Link key={item.product.idProduct} to={`/product-detail/${item.product.idProduct}`}>
-                <button>Ver lugar</button>
-                </Link>
+                <div className={ `card ${new Date(item.endDate).setDate(new Date(item.endDate).getDate()) < (new Date())? `inactive` : `active`} `}>
+                    <div className="card-header">
+                    <h3>{item.product.title}</h3>
+                    </div>
+                    <div className="card-body">
+                        <p className="card-body-item"> <span> <RiLoginCircleFill className="card-body-item-icon in" />  Check-in: </span> {item.startDate}</p>
+                        <hr />
+                        <p className="card-body-item"> <span> <TbClock2 className="card-body-item-icon"  /> Ingreso: </span> {item.startHour} Hs</p>
+                        <hr />
+                        <p className="card-body-item"> <span> <RiLogoutCircleFill className="card-body-item-icon out"  /> Check-out: </span> {item.endDate}</p>
+                    </div>
+                    <div className="card-footer">
+                    <Link key={item.product.idProduct} to={`/product-detail/${item.product.idProduct}`}>
+                    <button>Ver lugar</button>
+                    </Link>
+                    </div>
                 </div>
             </div>
         ))}
