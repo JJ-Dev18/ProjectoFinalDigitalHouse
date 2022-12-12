def59a2054d85f676d6411ee6abc3de09bcd96ed
jriveralopez <jairorive96@gmail.com>
2022-12-11 20:22:31 -0500

    Adding endpoints for product post UH

---
+++ BackEnd/DHBooking/src/main/java/Grupo7/DHBooking/Service/ISecurityPolityService.java
@@ -0,0 +1,10 @@
+package Grupo7.DHBooking.Service;
+
+import Grupo7.DHBooking.Entities.SecurityPolicy;
+
+import java.util.List;
+
+public interface ISecurityPolityService {
+
+    List<SecurityPolicy> getAll();
+}
