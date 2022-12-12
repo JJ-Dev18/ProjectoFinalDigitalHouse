47809b3f4b04f3d950c2f2ffc2ad8337cd6f3560
jriveralopez <jairorive96@gmail.com>
2022-12-11 19:31:22 -0500

    Adding policies controllers for product post

---
+++ BackEnd/DHBooking/src/main/java/Grupo7/DHBooking/Service/INormPolityService.java
@@ -0,0 +1,9 @@
+package Grupo7.DHBooking.Service;
+
+import Grupo7.DHBooking.Entities.NormPolicy;
+
+import java.util.List;
+
+public interface INormPolityService {
+    List<NormPolicy> getAll();
+}
