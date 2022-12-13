35c83b97ced1ce69135f23b76c4ed07fba04bd41
jriveralopez <jairorive96@gmail.com>
2022-12-13 00:58:28 -0500

    Adding advances for sprint 4

--- Testing/SPRINT 3--/tests jest/SuccesfullBooking.test.js
+++
@@ -1,9 +0,0 @@
-import SuccesfullBooking from "../components/SuccessfulBooking.jsx"
-import '@testing-library/jest-dom/extend-expect'
-import { render, screen } from '@testing-library/react'
-
-test("Se renderiza el mensaje de exito", ()=>{
-  render(<SuccesfullBooking/>);
-  const message = screen.getByText(/Su reserva se ha realizado con éxito/i);
-  expect(message).toBeInTheDocument()
-})
