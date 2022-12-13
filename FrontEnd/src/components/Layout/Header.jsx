d37691a5f591c5a4abbc670b15d7076d655b6272
jriveralopez <jairorive96@gmail.com>
2022-12-13 18:18:35 -0500

    Adding v.3.0 changes

--- FrontEnd/src/components/Layout/Header.jsx
+++ FrontEnd/src/components/Layout/Header.jsx
@@ -49,7 +49,7 @@ const Header = () => {
       />
       {auth ? (
         <div className="md-screen avatar">
-          {userAuth.role.idRole === 1 ?
+          {userAuth.role.idRole != null && userAuth.role.idRole ===1 ?
             <button>
               <Link to="/administrator">{menuItems[3]}</Link>
             </button>
@@ -101,7 +101,7 @@ const Header = () => {
           </div>
           <ul className="list">
           <li>
-              {userAuth.role.idRole === 1 ?
+              {userAuth.role.idRole != null && userAuth.role.idRole ===1 ?
                 <Link to="/administrator">{menuItems[3]}</Link>
                 :
                 <Link to="/my-bookings">{menuItems[2]}</Link>
